a242035a2c7e7e8519e83853330ebfbf724a83bf
SVRourke <samuelvanrourke@gmail.com>
2021-10-12 15:50:16 -0400

    added relationships to StockRecord

--- db/migrate/20211012184355_create_stock_records.rb
+++ db/migrate/20211012184355_create_stock_records.rb
@@ -2,7 +2,8 @@ class CreateStockRecords < ActiveRecord::Migration[6.1]
   def change
     create_table :stock_records do |t|
       t.boolean :available
-
+      t.belongs_to :merchant
+      t.belongs_to :card
       t.timestamps
     end
   end

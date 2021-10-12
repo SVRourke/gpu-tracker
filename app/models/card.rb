8cd47ce139c2828f3b07ceed2c6e27565e6e5775
SVRourke <samuelvanrourke@gmail.com>
2021-10-12 14:20:08 -0400

    fixed typos

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ApplicationRecord
-    has_many: StockRecords
-    has_many: Merchants, through: StockRecords
+    has_many :stockrecords
+    has_many :merchants, through: :stockrecords
 end

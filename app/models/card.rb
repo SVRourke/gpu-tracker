da64b1fe327c751f27c4f31a2fef78095b2e8628
SVRourke <samuelvanrourke@gmail.com>
2021-10-13 12:44:08 -0400

    added inStockMerchants method

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,12 @@
 class Card < ApplicationRecord
     has_many :stock_records
     has_many :merchants, through: :stock_records
+
+    def inStockMerchants
+        records = self.stock_records.where(available: true)
+        merchants = records.map do |record|
+            record.merchant
+        end
+        return merchants
+    end
 end

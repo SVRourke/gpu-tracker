aaf9eb11ae1edda4bd73558f47362508d1b39932
SVRourke <samuelvanrourke@gmail.com>
2021-10-13 12:45:20 -0400

    added tests for relationships with StockRecords and Merchants and the InStockMerchants method

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -34,6 +34,36 @@ RSpec.describe Card, type: :model do
     end
   end
 
-  pending "has many stock records"
-  pending "has many merchants"
+  describe "it has many" do
+    it "stock_records" do
+      card = Card.create(name: "GeForce GTX 760", manufacturer: "Nvidia", sku: "023942834a23480")
+      merchant = Merchant.create(name: "newegg", url: "https://www.newegg.com")
+      card.merchants.push(merchant)
+      
+      expect(card.stock_records.count).to equal 1
+      expect(card.stock_records.first.merchant).to eq(merchant)
+      
+    end
+    
+    it "merchants" do
+      merchant = Merchant.create(name: "newegg", url: "https://www.newegg.com")
+      card = Card.create(name: "GeForce GTX 760", manufacturer: "Nvidia", sku: "023942834a23480")
+      card.merchants.push(merchant)
+      
+      expect(card.merchants.count).to equal 1
+      expect(card.merchants.first).to eq(merchant)
+    end
+  end
+
+  describe "#inStockMerchants" do
+    it "returns a list of merchants that have the card in stock" do
+      merchant = Merchant.create(name: "newegg", url: "https://www.newegg.com")
+      card = Card.create(name: "GeForce GTX 760", manufacturer: "Nvidia", sku: "023942834a23480")
+      card.merchants.push(merchant)
+      card.stock_records.first().update(available: true)
+
+      expect(card.inStockMerchants().count()).to eq(1)
+      expect(card.inStockMerchants().first().name).to eq("newegg")
+    end
+  end
 end

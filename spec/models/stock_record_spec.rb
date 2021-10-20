1b0fb95f5cb382deb7e62196e1d7f21525fc81a4
SVRourke <samuelvanrourke@gmail.com>
2021-10-20 08:22:21 -0400

    created tests for stock record

--- spec/models/stock_record_spec.rb
+++ spec/models/stock_record_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe StockRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @card = Card.create(name: "GeForce GTX 760", manufacturer: "Nvidia", sku: "023942834a23480")
+    @merchant = Merchant.create(name: "newegg", url: "https://www.newegg.com")
+    @card.merchants.push(@merchant)
+  end
+
+  describe "Availablilty" do
+    it "is false by default" do
+      # byebug
+      expect(@card.stock_records.first().available).to be false      
+    end
+  end
+
+  describe "is invalid without a" do
+    it "merchant" do
+      sr = StockRecord.create(card: @card)
+      expect(sr.valid?).to be false
+    end
+    
+    it "card" do
+      sr = StockRecord.create(merchant: @merchant)
+      expect(sr.valid?).to be false
+    end
+
+  end
 end

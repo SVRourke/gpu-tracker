78454162b660d629ee8efe44cd2a4c9d2df27a6e
SVRourke <samuelvanrourke@gmail.com>
2021-11-06 07:45:48 -0400

    started stubbing out scraper class and strategy

---
+++ lib/scraper/scraper.rb
@@ -0,0 +1,13 @@
+class Scraper
+    @page = nil
+
+    def pull_page(url)
+        # download the page set the html to @page
+    end
+
+    def parse_info
+        # select the proper strategy and use it to 
+        # parse the important info
+        
+    end
+end
